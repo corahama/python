030b281b6a50a12949fb01b042a58824d01e90c0
Fernando Contreras <luisfernando_contreras@hotmail.com>
2022-05-09 20:44:38 -0500

    sort_recursive

---
+++ algoritmos/sort_recursive.py
@@ -0,0 +1,21 @@
+def sort_array(stack):
+    if len(stack) == 0:
+        return stack
+
+    top = stack.pop()
+    sort_array(stack)
+    insert_element_in_ordered_stack(stack, top)
+    return stack
+
+def insert_element_in_ordered_stack(stack, value):
+    if len(stack) == 0 or stack[-1] <= value:
+        stack.append(value)
+        return
+    top = stack.pop()
+    insert_element_in_ordered_stack(stack, value)
+    stack.append(top)
+
+
+
+if __name__ == '__main__':
+    print(sort_array([-5, 2, -2, 4, 3, 1]))
